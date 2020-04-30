a5f8b3453e36ff5cccafc98ce5b77afccf270d62
Sungcad <30380844+Sungcad@users.noreply.github.com>
2020-04-29 20:17:12 -0500

    fix bug when hammer is dropped from inventory

--- src/me/sungcad/repairhammers/costs/VaultHook.java
+++ src/me/sungcad/repairhammers/costs/VaultHook.java
@@ -10,22 +10,24 @@ import me.sungcad.repairhammers.RepairHammerPlugin;
 import net.milkbowl.vault.economy.Economy;
 
 public class VaultHook {
-    private Economy econ;
+	private Economy econ;
 
-    public VaultHook(RepairHammerPlugin plugin) {
-        if (Bukkit.getPluginManager().isPluginEnabled("Vault")) {
-            RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-            econ = economyProvider.getProvider();
-        } else {
-            plugin.getLogger().warning("Vault not found all hammers with a costs are free");
-        }
-    }
+	public VaultHook(RepairHammerPlugin plugin) {
+		if (Bukkit.getPluginManager().isPluginEnabled("Vault")) {
+			RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+			if (economyProvider != null) {
+				econ = economyProvider.getProvider();
+				return;
+			}
+		}
+		plugin.getLogger().warning("Vault not found all hammers with a costs are free");
+	}
 
-    public Economy getEconomy() {
-        return econ;
-    }
+	public Economy getEconomy() {
+		return econ;
+	}
 
-    public boolean isLoaded() {
-        return econ != null;
-    }
+	public boolean isLoaded() {
+		return econ != null;
+	}
 }
--- src/me/sungcad/repairhammers/listeners/InventoryClickListener.java
+++ src/me/sungcad/repairhammers/listeners/InventoryClickListener.java
@@ -18,63 +18,65 @@ import me.sungcad.repairhammers.hammers.Hammer;
 import me.sungcad.repairhammers.itemhooks.CustomItemHook;
 
 public class InventoryClickListener implements Listener {
-    private RepairHammerPlugin plugin;
-    private static boolean enabled;
+	private RepairHammerPlugin plugin;
+	private static boolean enabled;
 
-    public InventoryClickListener(RepairHammerPlugin plugin, boolean enable) {
-        this.plugin = plugin;
-        enabled = enable;
-    }
+	public InventoryClickListener(RepairHammerPlugin plugin, boolean enable) {
+		this.plugin = plugin;
+		enabled = enable;
+	}
 
-    public static void setEnabeld(boolean enable) {
-        enabled = enable;
-    }
+	public static void setEnabeld(boolean enable) {
+		enabled = enable;
+	}
 
-    @EventHandler
-    public void onClickEvent(InventoryClickEvent e) {
-        if (!enabled)
-            return;
-        Player player = (Player) e.getWhoClicked();
-        if (!player.getOpenInventory().getType().equals(InventoryType.CRAFTING))
-            return;
-        ItemStack cursor = e.getCursor();
-        Optional<Hammer> ohammer = plugin.getHammerManager().getHammer(cursor);
-        if (!ohammer.isPresent())
-            return;
-        Hammer hammer = ohammer.get();
-        if (!hammer.canUse(player))
-            return;
-        ItemStack target = e.getCurrentItem();
-        CustomItemHook hook = plugin.getCustomItemManager().getHook(target);
-        if (hook == null)
-            return;
-        if (!hammer.canFix(target))
-            return;
-        int damage;
-        if (hammer.isPercent())
-            damage = (int) Math.min(hook.getMaxDurability(target) * hammer.getFixAmount() * .01, hook.getDamage(target));
-        else
-            damage = Math.min(hook.getDamage(target), hammer.getFixAmount(cursor));
-        if (damage <= 0)
-            return;
-        e.setCancelled(true);
-        if (hammer.canAfford(player, false)) {
-            hammer.payCost(player, false);
-        } else {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("error.bal.use").replace("<cost>", plugin.getFormat().format(hammer.getUseCost()))));
-            return;
-        }
-        if (hammer.useDurability(cursor, damage) == null)
-            player.setItemOnCursor(null);
-        if (hammer.isFixall()) {
-            for (ItemStack item : player.getInventory()) {
-                if (hammer.canFix(item)) {
-                    plugin.getCustomItemManager().fixItem(item, damage);
-                }
-            }
-        } else {
-            hook.fixItem(target, damage);
-        }
-        hammer.getUseMessage().forEach(line -> player.sendMessage(ChatColor.translateAlternateColorCodes('&', line)));
-    }
+	@EventHandler
+	public void onClickEvent(InventoryClickEvent e) {
+		if (!enabled)
+			return;
+		Player player = (Player) e.getWhoClicked();
+		if (!player.getOpenInventory().getType().equals(InventoryType.CRAFTING))
+			return;
+		ItemStack cursor = e.getCursor();
+		Optional<Hammer> ohammer = plugin.getHammerManager().getHammer(cursor);
+		if (!ohammer.isPresent())
+			return;
+		Hammer hammer = ohammer.get();
+		if (!hammer.canUse(player))
+			return;
+		ItemStack target = e.getCurrentItem();
+		if (target == null)
+			return;
+		CustomItemHook hook = plugin.getCustomItemManager().getHook(target);
+		if (hook == null)
+			return;
+		if (!hammer.canFix(target))
+			return;
+		int damage;
+		if (hammer.isPercent())
+			damage = (int) Math.min(hook.getMaxDurability(target) * hammer.getFixAmount() * .01, hook.getDamage(target));
+		else
+			damage = Math.min(hook.getDamage(target), hammer.getFixAmount(cursor));
+		if (damage <= 0)
+			return;
+		e.setCancelled(true);
+		if (hammer.canAfford(player, false)) {
+			hammer.payCost(player, false);
+		} else {
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("error.bal.use").replace("<cost>", plugin.getFormat().format(hammer.getUseCost()))));
+			return;
+		}
+		if (hammer.useDurability(cursor, damage) == null)
+			player.setItemOnCursor(null);
+		if (hammer.isFixall()) {
+			for (ItemStack item : player.getInventory()) {
+				if (hammer.canFix(item)) {
+					plugin.getCustomItemManager().fixItem(item, damage);
+				}
+			}
+		} else {
+			hook.fixItem(target, damage);
+		}
+		hammer.getUseMessage().forEach(line -> player.sendMessage(ChatColor.translateAlternateColorCodes('&', line)));
+	}
 }
